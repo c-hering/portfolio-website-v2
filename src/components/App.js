07eaae6aec80a41740b86c9a1b0920b23eab7684
An0rak <phos4es@gmail.com>
2017-05-29 01:29:28 -0400

    found it

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ProjectBoard from './ProjectBoard.js';
 import NavBar from './NavigationBar.js';
 import ProcGroup from './ProcGroup.js';
+import Waves from '../../public/images/waves.png';
 import '../style/App.css';
 
 export default class App extends Component {
@@ -11,16 +12,13 @@ export default class App extends Component {
         <div className="App-header">
           <NavBar />
           <div id="top-content-wrapper">
-            <div id="top-title-wrapper">
-              <h1 id="top-title" className="content-title">
-                Let&#39;s Create Together
-              </h1>
-            </div>
-            <ProcGroup />
-            <button type="button" id="launch-button" className="button sliding-middle-out">GO</button>
+            <img id="top-waves" src={Waves}></img>
+            <h1 id="top-content-title">Make it <br/><span className="highlights">Personal</span></h1>
           </div>
         </div>
         <div id="middle-content-wrapper">
+          <ProcGroup />
+          <button type="button" id="launch-button" className="button sliding-middle-out">GO</button>
         </div>
       </div>
     );
